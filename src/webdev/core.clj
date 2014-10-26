ee4490b7a3d0e7b8b37c1c0992bc36066e2401e1
Grayson Carroll <carrollgt91@gmail.com>
2014-10-26 14:55:42 -0500

    Adds handler dump

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -1,6 +1,7 @@
 (ns webdev.core
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
+            [ring.handler.dump :refer [handle-dump]]
             [compojure.core :refer [defroutes GET]]
             [compojure.route :refer [not-found]]))
 
@@ -14,9 +15,15 @@
    :body "goodbye, cruel world"
    :headers {}})
 
+(defn request [req]
+  {:status 200
+   :body (str req)
+   :headers {}})
+
 (defroutes app
   (GET "/" [] greet)
   (GET "/goodbye" [] goodbye)
+  (GET "/request" [] handle-dump)
   (not-found "Page not found."))
 
 (defn -main [port]
